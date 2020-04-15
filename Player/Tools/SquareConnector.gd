9104528d11b1b7efe0ef806b63998184afb3349d
cozyExploration <cozy.exploration@gmail.com>
2020-04-15 09:25:06 +0900

    (Unstable: node without const Type will occur abort of program) SquareConnector works more faster by filtering synapse node.

--- Player/Tools/SquareConnector.gd
+++ Player/Tools/SquareConnector.gd
@@ -61,7 +61,11 @@ func initiate() -> void:
 		var Bbodies = BareaDetector.get_overlapping_bodies()
 
 		for Abody in Abodies:
+			if Abody.Type == G.N_Types.N_Synapse:
+				continue
 			for Bbody in Bbodies:
+				if Bbody.Type == G.N_Types.N_Synapse:
+					continue
 				var newSynapse = get_parent().get_node("PointConnector").use2(Abody, Bbody)
 				if newSynapse != null:
 					G.default_session.add_child(newSynapse)
